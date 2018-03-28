653c3fa402d88c343322cab616baaaebf94e418e
Yoon Jo Han <yuniegu3@gmail.com>
2018-03-28 16:08:53 -0400

    playing around with js

--- main.js
+++ main.js
@@ -1,15 +1,56 @@
-const pokemon = {
+
+
+
+
+
+
+
+
+// class Child {
+// 	constructor(firstName, lastName, age) {
+// 	  this.firstName = firstName
+// 	  this.lastName = lastName
+// 	  this.age = age
+// 	}
+//   }
+  
+//   class DayCare {
+// 	constructor() { 
+// 	  this.children = []
+// 	}
+	
+// 	add(child) {
+// 	  this.children.push(child)
+// 	}
+	
+// 	displayAll() {
+// 	  for(let i = 0; i < this.children.length; ++i) {
+// 		console.log(this.children[i])
+// 	  }
+// 	}
+//   }
+  
+//   let orlando = new Child("orlando", "c", 6)
+//   let denisse = new Child("denisse", "c", -5)
+  
+//   let newDayCare = new DayCare()
+  
+//   newDayCare.add(orlando)
+//   newDayCare.add(denisse)
+  
+//   newDayCare.displayAll()
+
+
+
+const callPokemon = {
 	callGrowlithe : function(){
 		let pic = $('#pic').attr('src', 'images/growlithe.png')
 		$.ajax({
 		type:'GET',
 		url:'https://pokeapi.co/api/v2/pokemon/58/',
 		success:function(data){
-			let pokname = (data.name);
-			$('#pokname').text(pokname);
-			//upper case thing is not working
-			// let caps = $('#pokname').attr('string').toUpperCase;
-			// $('pokename').append;
+			let name = (data.name);
+			$('#name').text(name);
 			let hpstat = (data.stats[5].base_stat);
 			$('#hpstat').text(hpstat);
 			let defstat = (data.stats[3].base_stat);
@@ -34,8 +75,8 @@ const pokemon = {
 		type:'GET',
 		url:'https://pokeapi.co/api/v2/pokemon/133/',
 		success:function(data){
-			let pokname = (data.name);
-			$('#pokname').text(pokname);
+			let name = (data.name);
+			$('#name').text(name);
 			let hpstat = (data.stats[5].base_stat);
 		   $('#hpstat').text(hpstat);
 		   let defstat = (data.stats[3].base_stat);
@@ -60,8 +101,8 @@ const pokemon = {
  	type:'GET',
  	url:'https://pokeapi.co/api/v2/pokemon/359/',
  	success:function(data){
- 		let pokname = (data.name);
- 		$('#pokname').text(pokname);
+		let name = (data.name);
+		$('#name').text(name);
  		let hpstat = (data.stats[5].base_stat);
 		$('#hpstat').text(hpstat);
 		let defstat = (data.stats[3].base_stat);
@@ -82,11 +123,28 @@ const pokemon = {
 	},
  };
 
+//  let growlithe = callPokemon.callGrowlithe();
+//  let eevee = callPokemon.callEevee();
+//  let absol = callPokemon.callAbsol();
+
+//  class MyTrainer {
+// 	callPokemon.callGrowlithe = growlithe,
+// 	callPokemon.callEevee = eevee,
+// 	callPokemon.callAbsol = absol,
+// 	}	
+
+// 	// callPokemon.callGrowlithe(); {
+// 	// 	console.log ('growlithe')
+// 	// }
+// }
+
+// class Pokemon {
+// 	g
+	
+// 	}
 
 
-
-
-const trainer = { };
+// const trainer = { };
 
 
 
