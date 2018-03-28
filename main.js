eb0ede6835ed7aec9fa7dab0bff02612820f3249
Yoon Jo Han <yuniegu3@gmail.com>
2018-03-28 15:01:51 -0400

    played around with the js, the ajex calls are stored in obj pokemon

--- main.js
+++ main.js
@@ -1,61 +1,61 @@
-let callGrowlithe =function(){
-	let pic = $('#pic').attr('src', 'growlithe.png')
-	$.ajax({
-	type:'GET',
-	url:'https://pokeapi.co/api/v2/pokemon/58/',
-	success:function(data){
-		let pokname = (data.name);
-		$('#pokname').text(pokname);
-		//upper case thing is not working
-		// $('#pokname').val($('#pokname').val().toUpperCase());
-		let hpstat = (data.stats[5].base_stat);
-		$('#hpstat').text(hpstat);
-		let defstat = (data.stats[3].base_stat);
-		$('#defstat').text(defstat);
-		let atkstat = (data.stats[4].base_stat);
-		$('#atkstat').text(atkstat);
-		let abili = (data.abilities[0].ability.name);
-		$('#abili').text(abili);
-		let abili2 = (data.abilities[1].ability.name);
-		$('#abili2').text(abili2);
-		let abili3 = (data.abilities[2].ability.name);
-		$('#abili3').text(abili3);
+const pokemon = {
+	callGrowlithe : function(){
+		let pic = $('#pic').attr('src', 'images/growlithe.png')
+		$.ajax({
+		type:'GET',
+		url:'https://pokeapi.co/api/v2/pokemon/58/',
+		success:function(data){
+			let pokname = (data.name);
+			$('#pokname').text(pokname);
+			//upper case thing is not working
+			// let caps = $('#pokname').attr('string').toUpperCase;
+			// $('pokename').append;
+			let hpstat = (data.stats[5].base_stat);
+			$('#hpstat').text(hpstat);
+			let defstat = (data.stats[3].base_stat);
+			$('#defstat').text(defstat);
+			let atkstat = (data.stats[4].base_stat);
+			$('#atkstat').text(atkstat);
+			let abili = (data.abilities[0].ability.name);
+			$('#abili').text(abili);
+			let abili2 = (data.abilities[1].ability.name);
+			$('#abili2').text(abili2);
+			let abili3 = (data.abilities[2].ability.name);
+			$('#abili3').text(abili3);
+		}, 
+		error:function(error){
+			console.log('There seems to be an error')
+		}
+		})
 	},
-	error:function(error){
-		console.log('There seems to be an error')
-	}
-	})
- };
-
- let callEevee = function(){
- 	let pic = $('#pic').attr('src', 'eevee.png')
- 	$.ajax({
- 	type:'GET',
- 	url:'https://pokeapi.co/api/v2/pokemon/133/',
- 	success:function(data){
- 		let pokname = (data.name);
- 		$('#pokname').text(pokname);
- 		let hpstat = (data.stats[5].base_stat);
-		$('#hpstat').text(hpstat);
-		let defstat = (data.stats[3].base_stat);
-		$('#defstat').text(defstat);
-		let atkstat = (data.stats[4].base_stat);
-		$('#atkstat').text(atkstat);
-		let abili = (data.abilities[0].ability.name);
-		$('#abili').text(abili);
-		let abili2 = (data.abilities[1].ability.name);
-		$('#abili2').text(abili2);
-		let abili3 = (data.abilities[2].ability.name);
-		$('#abili3').text(abili3);
- 	},
- 	error:function(error){
- 		console.log('There seems to be an error')
- 	}
- 	})
- };
-
- let callAbsol = function(){
- 	let pic = $('#pic').attr('src', 'absol.png')
+	callEevee : function(){
+		let pic = $('#pic').attr('src', 'images/eevee.png')
+		$.ajax({
+		type:'GET',
+		url:'https://pokeapi.co/api/v2/pokemon/133/',
+		success:function(data){
+			let pokname = (data.name);
+			$('#pokname').text(pokname);
+			let hpstat = (data.stats[5].base_stat);
+		   $('#hpstat').text(hpstat);
+		   let defstat = (data.stats[3].base_stat);
+		   $('#defstat').text(defstat);
+		   let atkstat = (data.stats[4].base_stat);
+		   $('#atkstat').text(atkstat);
+		   let abili = (data.abilities[0].ability.name);
+		   $('#abili').text(abili);
+		   let abili2 = (data.abilities[1].ability.name);
+		   $('#abili2').text(abili2);
+		   let abili3 = (data.abilities[2].ability.name);
+		   $('#abili3').text(abili3);
+		},
+		error:function(error){
+			console.log('There seems to be an error')
+		}
+		})
+	},
+	callAbsol : function(){
+ 	let pic = $('#pic').attr('src', 'images/absol.png')
  	$.ajax({
  	type:'GET',
  	url:'https://pokeapi.co/api/v2/pokemon/359/',
@@ -78,11 +78,144 @@ let callGrowlithe =function(){
  	error:function(error){
  		console.log('There seems to be an error')
  	}
- 	})
+	 })
+	},
  };
- 
-// let hugo = {
-// 	growlithe: callGrowlithe();
+
+
+
+
+
+const trainer = { };
+
+
+
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+
+
+
+// let callGrowlithe = function(){
+// 	let pic = $('#pic').attr('src', 'images/growlithe.png')
+// 	$.ajax({
+// 	type:'GET',
+// 	url:'https://pokeapi.co/api/v2/pokemon/58/',
+// 	success:function(data){
+// 		let pokname = (data.name);
+// 		$('#pokname').text(pokname);
+// 		//upper case thing is not working
+// 		// let caps = $('#pokname').attr('string').toUpperCase;
+// 		// $('pokename').append;
+// 		let hpstat = (data.stats[5].base_stat);
+// 		$('#hpstat').text(hpstat);
+// 		let defstat = (data.stats[3].base_stat);
+// 		$('#defstat').text(defstat);
+// 		let atkstat = (data.stats[4].base_stat);
+// 		$('#atkstat').text(atkstat);
+// 		let abili = (data.abilities[0].ability.name);
+// 		$('#abili').text(abili);
+// 		let abili2 = (data.abilities[1].ability.name);
+// 		$('#abili2').text(abili2);
+// 		let abili3 = (data.abilities[2].ability.name);
+// 		$('#abili3').text(abili3);
+// 	}, 
+// 	error:function(error){
+// 		console.log('There seems to be an error')
+// 	}
+// 	})
+//  };
+
+//  let callEevee = function(){
+//  	let pic = $('#pic').attr('src', 'images/eevee.png')
+//  	$.ajax({
+//  	type:'GET',
+//  	url:'https://pokeapi.co/api/v2/pokemon/133/',
+//  	success:function(data){
+//  		let pokname = (data.name);
+//  		$('#pokname').text(pokname);
+//  		let hpstat = (data.stats[5].base_stat);
+// 		$('#hpstat').text(hpstat);
+// 		let defstat = (data.stats[3].base_stat);
+// 		$('#defstat').text(defstat);
+// 		let atkstat = (data.stats[4].base_stat);
+// 		$('#atkstat').text(atkstat);
+// 		let abili = (data.abilities[0].ability.name);
+// 		$('#abili').text(abili);
+// 		let abili2 = (data.abilities[1].ability.name);
+// 		$('#abili2').text(abili2);
+// 		let abili3 = (data.abilities[2].ability.name);
+// 		$('#abili3').text(abili3);
+//  	},
+//  	error:function(error){
+//  		console.log('There seems to be an error')
+//  	}
+//  	})
+//  };
+
+//  let callAbsol = function(){
+//  	let pic = $('#pic').attr('src', 'images/absol.png')
+//  	$.ajax({
+//  	type:'GET',
+//  	url:'https://pokeapi.co/api/v2/pokemon/359/',
+//  	success:function(data){
+//  		let pokname = (data.name);
+//  		$('#pokname').text(pokname);
+//  		let hpstat = (data.stats[5].base_stat);
+// 		$('#hpstat').text(hpstat);
+// 		let defstat = (data.stats[3].base_stat);
+// 		$('#defstat').text(defstat);
+// 		let atkstat = (data.stats[4].base_stat);
+// 		$('#atkstat').text(atkstat);
+// 		let abili = (data.abilities[0].ability.name);
+// 		$('#abili').text(abili);
+// 		let abili2 = (data.abilities[1].ability.name);
+// 		$('#abili2').text(abili2);
+// 		let abili3 = (data.abilities[2].ability.name);
+// 		$('#abili3').text(abili3);
+//  	},
+//  	error:function(error){
+//  		console.log('There seems to be an error')
+//  	}
+//  	})
+//  };	
+
+
+//  const pokemon = {
+// 	growlithe: callGrowlithe(),
+// 	absol: callAbsol(),
+// 	eevee: callEevee(),
 // }
+//trying out a search input that will pull the pokemon searched from the pokemon api.
 
+//  let inputname = $('input').val();
+//  let callPokemons = function(name){
+//  	$.ajax({
+//  	type:'GET',
+//  	url:'https://pokeapi.co/api/v2/pokemon/',
+//  	success:function(data){
+// 		if inputname = (data.results.name)
+// 		let pokname = (data.name)
+//  		$('#pokname').text(pokname);
+//  		let hpstat = (data.stats[5].base_stat);
+// 		$('#hpstat').text(hpstat);
+// 		let defstat = (data.stats[3].base_stat);
+// 		$('#defstat').text(defstat);
+// 		let atkstat = (data.stats[4].base_stat);
+// 		$('#atkstat').text(atkstat);
+// 		let abili = (data.abilities[0].ability.name);
+// 		$('#abili').text(abili);
+// 		let abili2 = (data.abilities[1].ability.name);
+// 		$('#abili2').text(abili2);
+// 		let abili3 = (data.abilities[2].ability.name);
+// 		$('#abili3').text(abili3);
+		 
+//  	},
+//  	error:function(error){
+//  		console.log('There seems to be an error')
+//  	}
+//  	})
+//  };
+ 
+ 
 
+// });
